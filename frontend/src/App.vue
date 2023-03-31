74bd89a3ca3eab9aa4b1a477e15095d63a5ce6c5
huangchunyan <qingyanxiaonian@163.com>
2023-03-31 16:38:13 +0800

    minor,add license

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,20 @@
+<!--
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
 <template>
   <div class="kyligence-zen-ml-toolkit">
     <div class="upload-box" v-if="!dowloadFileUrl" :loading="isUploading">
@@ -65,7 +82,6 @@ export default {
       this.dowloadFileUrl = '/download?uuid=' + res.uuid
     },
     handleError (err, file, fileList) {
-      // error 是个 Error 对象，所以要获取内部的内容，需要用 err.message 取出，可参考 js 对 new Error 的文档说明
       let msg = ''
       try {
         const errorJson = JSON.parse(err.message)
